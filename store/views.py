01713eda798bd3860668a91c60b1a894cf9f5313
Mario437619 <marioj3@gmail.com>
2025-12-14 01:59:31 -0600

    decimo commit

--- store/views.py
+++ store/views.py
@@ -478,7 +478,7 @@ def multi_sale(request):
             total=total,
             status='completed',
             payment_method='cash',
-            payment_status='paid',
+            payment_status='completed',
             created_at=now,
             updated_at=now
         )
